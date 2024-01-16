e9cc8adad0c2a45f2e32e16d9b77891a3220744b
Casey R <crowlandsdev@gmail.com>
2024-01-16 08:56:24 -0500

    added javadoc comments to the recursiveFib method

--- fibonacci/src/Main.java
+++ fibonacci/src/Main.java
@@ -7,19 +7,26 @@ public class Main {
         int k = x % 100;
 
         if (j == 1 &&  k != 11) {
-            System.out.println("The " + x + "th number in the finbonacci sequence is " + y);
+            System.out.println("The " + x + "th number in the fibonacci sequence is " + y);
         }
         else if (j == 2 && k != 12) {
-            System.out.println("The " + x + "nd number in the finbonacci sequence is " + y);
+            System.out.println("The " + x + "nd number in the fibonacci sequence is " + y);
         }
         else if (j == 3 && k != 13) {
-            System.out.println("The " + x + "rd number in the finbonacci sequence is " + y);
+            System.out.println("The " + x + "rd number in the fibonacci sequence is " + y);
         }
         else {
-            System.out.println("The " + x + "th number in the finbonacci sequence is " + y);
+            System.out.println("The " + x + "th number in the fibonacci sequence is " + y);
         }
     }
 
+    /**
+     * Returns the nth number in the Fibonacci sequence using recursion.
+     * If n is less than or equal to 1, n is returned instead
+     *
+     * @param n represents the nth integer in the fibonacci sequence
+     * @return int
+     */
     public static int recursiveFib(int n) {
         if(n <= 1) {
             return n;
